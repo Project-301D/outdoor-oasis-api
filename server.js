aacdb498eb54107683d7aca3a82a0d93be4995e3
Luke McCarthy <astartofanewlight@gmail.com>
2023-11-06 14:27:48 -0600

    added server

--- server.js
+++ server.js
@@ -1,2 +1,33 @@
 'use strict';
-console.log('working');
+console.log('server file is connected');
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 5555;
+
+
+
+
+app.get('/', (req, res) => res.status(200).send('Hello from the Server!'));
+
+
+
+
+
+app.get('*', (req,res) => {
+  res.status(404).send('Not Found');
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
+
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
