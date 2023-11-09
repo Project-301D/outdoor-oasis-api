719ad44b06ac1d22dbbca0db544727ce813fcad5
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-11-09 15:42:27 -0600

    removed clutter

--- modules/campground.js
+++ modules/campground.js
@@ -9,24 +9,12 @@ async function getCampGround(parkCode, description, name) {
   //where it's parks in the url it was campgrounds
   let url = `https://developer.nps.gov/api/v1/parks/?api_key=${process.env.CAMP_API_KEY}&q=${name}`;
   let result = await axios.get(url)
-    .then(apiResponse => parseCampGround2(apiResponse.data.data));
+    .then(apiResponse => parseCampGround(apiResponse.data.data));
   // console.log('result: ', result);
   return result;
 }
 
-// function parseCampGround(campData) {
-//   try {
-//     console.log(campData[0].contacts.phoneNumbers);
-//     const campSummaries = campData[0].contacts.phoneNumbers.map(campground => {
-//       return new Campground(campground);
-//     });
-//     return Promise.resolve(campSummaries);
-//   } catch (error) {
-//     return Promise.reject(error);
-//   }
-// }
-
-function parseCampGround2(campData) {
+function parseCampGround(campData) {
   try {
     campData.sort((a,b) => b.relevanceScore - a.relevanceScore);
     let mostRelevantCamp = campData[0];
@@ -42,7 +30,7 @@ function parseCampGround2(campData) {
     console.log('phone?: ', relevantArray[0].phone);
     console.log('email?: ', relevantArray[0].email);
     const campSummaries = relevantArray.map(campground => {
-      return new Campground2(campground);
+      return new Campground(campground);
     });
     return Promise.resolve(campSummaries);
   } catch (error) {
@@ -50,14 +38,7 @@ function parseCampGround2(campData) {
   }
 }
 
-// class Campground {
-//   constructor(data) {
-//     this.phoneNumber = data.phoneNumber;
-//     this.type = data.type;
-//   }
-// }
-
-class Campground2 {
+class Campground {
   constructor(data) {
     this.latitude = data.latitude;
     this.longitude = data.longitude;
